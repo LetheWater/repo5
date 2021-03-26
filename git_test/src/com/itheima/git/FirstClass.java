45faed8fc43dd7caf8ee1a807836b084667e6455
弈峰 <1436195248@qq.com>
2021-03-26 19:48:57 +0800

    修改

--- git_test/src/com/itheima/git/FirstClass.java
+++ git_test/src/com/itheima/git/FirstClass.java
@@ -12,6 +12,7 @@ public class FirstClass {
         System.out.println("hello Git project");
         System.out.println("hello Git project");
         //修改修改修改
+        System.out.println("sss");
     }
     public void test() {
         System.out.println("hello t");
