eed4bb72274108d1f94091248ae1d9b62ed1e987
弈峰 <1436195248@qq.com>
2021-03-26 19:23:06 +0800

    添加工程

---
+++ git_test/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,15 @@
+package com.itheima.git;
+
+public class FirstClass {
+    public static void main(String[] agrs) {
+        System.out.println("hello Git project");
+        System.out.println("hello Git project");
+        System.out.println("hello Git project");
+        System.out.println("hello Git project");
+        System.out.println("hello Git project");
+        System.out.println("hello Git project");
+        System.out.println("hello Git project");
+        System.out.println("hello Git project");
+        System.out.println("hello Git project");
+    }
+}
